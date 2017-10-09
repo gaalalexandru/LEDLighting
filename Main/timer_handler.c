d1fc6c47c92edef1dd675880189949a37dabf68c
Gaal Alexandru <gsx_16@yahoo.com>
2017-10-10 00:34:17 +0300

    setup timer2

--- Main/timer_handler.c
+++ Main/timer_handler.c
@@ -20,7 +20,7 @@
 /* Timer0 - 8bit Initialization function*/
 void TIMER0_Init(void)
 {
-	TCCR0 |= (1 << CS02)|(1 << CS00);  //clk/1024 (From prescaler)
+	TCCR0 = (1 << CS02)|(1 << CS00);  //clk/1024 (From prescaler)
 	TCNT0 = 0;
 	TIMSK  |= (1 << TOIE0);  //Enable Timer0 overflow interrupt TOIE0
 }
@@ -29,10 +29,19 @@ void TIMER0_Init(void)
 void TIMER1_Init(void)
 {
 	TCCR1A = 0;
-	TCCR1B |= (1 << WGM12)|(1 << CS00);  //clk/254 (From prescaler)
+	TCCR1B = (1 << WGM12)|(1 << CS00);  //clk/254 (From prescaler)
 	TCNT1 = 0;
 	OCR1A = 500;
 	TIMSK  |= (1 << OCIE1A);  //Enable Timer1 output compare trigger OCIE1A
+}
+
+/* Timer2 - 8bit Initialization function*/
+void TIMER2_Init(void)
+{
+	TCCR2 = (1 << WGM21)|(1 << CS01)|(1 << CS00);  //prescaler = 32, CTC mode, OC pin disabled
+	TCNT2 = 0;
+	OCR2 = 125;
+	TIMSK  |= (1 << OCIE2);  //Enable Timer1 output compare trigger OCIE1A
 }
 /*
 unsigned int TIM16_ReadTCNT1( void )
@@ -47,8 +56,25 @@ unsigned int TIM16_ReadTCNT1( void )
 }
 */
 
+/* Timer2 Compare MatchInterrupt function*/
+ISR (TIMER2_COMP_vect)
+{
+	unsigned char pulse_width = 10;
+	static unsigned char counter = 0;
+	if (counter <= pulse_width)
+	{
+		PORTD = Pin6;
+	}
+	else
+	{
+		PORTD = 0x00;
+	}
+	counter = 1+(counter%100);
+}
+
 /* Timer1 Compare Match A Interrupt function*/
-ISR (TIMER1_COMPA_vect) // timer0 overflow interrupt
+/*
+ISR (TIMER1_COMPA_vect)
 {
 	static unsigned char ledState = 0;
 	if(ledState)
@@ -61,9 +87,11 @@ ISR (TIMER1_COMPA_vect) // timer0 overflow interrupt
 	}
 	ledState ^= 1;
 }
+*/
 
-/* Timer0 Overflow Interrupt function*//*
-ISR (TIMER0_OVF_vect) // timer0 overflow interrupt
+/* Timer0 Overflow Interrupt function*/
+/*
+ISR (TIMER0_OVF_vect)
 {
 	static unsigned char ledState = 0;
 	if(ledState)
@@ -80,6 +108,7 @@ ISR (TIMER0_OVF_vect) // timer0 overflow interrupt
 	}
 	ledState ^= 1;
 
-}*/
+}
+*/
 
 //OSCCAL
