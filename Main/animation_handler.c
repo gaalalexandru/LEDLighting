a65d42bd70e6994db93a82c334090a8179d83473
Bogdan Raț <bogdanalexandru.rat@gmail.com>
2018-03-09 11:33:56 +0200

    Basic animation
    Animation for startup and lost/regain connection

---
+++ Main/animation_handler.c
@@ -0,0 +1,53 @@
+/*
+ * animation_handler.c
+ *
+ * Created: 3/7/2018 10:52:10 AM
+ *  Author: bogdan.rat
+ */ 
+
+#include <stdbool.h>
+#include "configuration.h"
+#include "animation_handler.h"
+#include "timer_handler.h"
+
+#define true	1
+#define false	0
+
+extern volatile uint8_t pwm_width_buffer[CHMAX];
+
+void animation_init(void)
+{
+	uint8_t i;
+	#if (STARTUP_ANIMATION_MODE == 1)
+	for(i = 0; i < CHMAX; ++i)
+	{
+		timer_delay_ms(100);
+		pwm_width_buffer[i] = STARTUP_ANIMATION_DEFAULTPWM;
+	}
+	
+	#elif (STARTUP_ANIMATION_MODE == 2)
+	for(uint8_t j = PWM_DUTY_CYCLE_RESET_VALUE; j <= STARTUP_ANIMATION_DEFAULTPWM; ++j)
+	{
+		timer_delay_ms(100);
+		for(i = 0; i < CHMAX; ++i)
+		{
+			pwm_width_buffer[i] = j;
+		}
+	}
+	#endif
+}
+
+void animation_setallchannels(const bool level)
+{
+	for(uint8_t i = 0; i < CHMAX; ++i)
+	{
+		if(level)
+		{
+			pwm_width_buffer[i] = 0;
+		}
+		else
+		{
+			pwm_width_buffer[i] = NOCONNECTION_ANIMATION_DEFAULTPWM;
+		}
+	}
+}
---
+++ Main/animation_handler.h
@@ -0,0 +1,17 @@
+/*
+ * animation_handler.h
+ *
+ * Created: 3/7/2018 10:49:58 AM
+ *  Author: bogdan.rat
+ */ 
+
+
+#ifndef ANIMATION_HANDLER_H_
+#define ANIMATION_HANDLER_H_
+
+#include <stdbool.h>
+
+void animation_init(void);
+void animation_setallchannels(const bool level);
+
+#endif /* ANIMATION_HANDLER_H_ */
